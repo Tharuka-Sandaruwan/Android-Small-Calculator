79b4ead274df21e117dbfc34d0c2c7c97cdb8a14
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-06 22:16:24 +0530

    substraction coded

--- app/src/main/java/com/example/smallcalculator/MainActivity.java
+++ app/src/main/java/com/example/smallcalculator/MainActivity.java
@@ -43,5 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button subBtn = (Button) findViewById(R.id.subBtn);
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num1.getText().toString().equals("") && num2.getText().toString().equals("")){
+                    results.setText("0");
+                }else if(num1.getText().toString().equals("")){
+                    results.setText("-"+num2.getText().toString());
+                }else if(num2.getText().toString().equals("")){
+                    results.setText(num1.getText().toString());
+                }
+                else{
+                    int no1= Integer.parseInt(num1.getText().toString());
+                    int no2= Integer.parseInt(num2.getText().toString());
+                    results.setText(String.valueOf(no1-no2));
+                }
+            }
+        });
+
+        Button mulBtn
     }
 }
